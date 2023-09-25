14a2ac0bc99bbf345f6487ff1c5e84f858548504
neha <admin@Admins-MacBook-Pro.local>
2023-09-25 09:33:01 +0530

    added all files to git

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+import { createApi } from 'unsplash-js';
+const main = document.querySelector('#main');
+const unsplash = createApi({
+    accessKey: "qrKFtvSv8o75ROTknSFmcYIUj_zlPTn23f1tI4LrDzc"
+})
+
+unsplash.search.getPhotos({
+    query: 'Classic Art',
+    page: 1,
+    perPage: 12,
+    orientation: 'portrait'
+}).then(result => {
+    if (result.type == 'success') {
+        const photos = result.response.results;
+        console.log(photos);
+        const getUrls = photos.map((i) => {
+            return `<img src="${i.urls.small}" />`;
+        });
+        main.innerHTML = getUrls.join('');
+    }
+});
